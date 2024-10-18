0a7cf5e740bf6a2d3e294cddf1fc79076e8841d6
Sergey K. <mock@email.zxc>
2024-10-18 23:15:50 +0300

    Добавлены алгоритмы для сжатия jpeg, png

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import styles from './App.module.css';
 import cn from 'clsx';
 import { optimize } from 'svgo';
+import imageCompression from 'browser-image-compression';
 
 export const App: React.FC = () => {
   const [dragging, setDragging] = useState(false);
-  const [svgContent, setSvgContent] = useState<string | null>(null);
-  const [compressedSvg, setCompressedSvg] = useState<string | null>(null);
-  const [compressionProgress, setCompressionProgress] = useState<number>(0);
+  const [compressedFile, setCompressedFile] = useState<Blob | null>(null);
+  const [progress, setProgress] = useState<number>(0);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const fileRef = React.useRef({ name: '' });
 
   const handleDragOver = (e: React.DragEvent) => {
     e.preventDefault();
@@ -19,56 +20,56 @@ export const App: React.FC = () => {
     setDragging(false);
   };
 
-  const handleDrop = (e: React.DragEvent) => {
+  const handleDrop = async (e: React.DragEvent) => {
     e.preventDefault();
     setDragging(false);
-    setCompressionProgress(0);
+    setProgress(0);
     setErrorMessage(null);
     const file = e.dataTransfer.files[0];
 
+    if (!file) return;
+
+    fileRef.current = { name: file.name };
+
     if (file && file.type === 'image/svg+xml') {
       const reader = new FileReader();
-      reader.onload = (event) => {
-        const svgData = event.target?.result as string;
-        setSvgContent(svgData);
-        compressSvg(svgData);
+      reader.onload = async (event: ProgressEvent<FileReader>) => {
+        setProgress(10);
+        const svgString = event.target?.result as string;
+        const compressedSvg = optimize(svgString, { multipass: true, floatPrecision: 2 }).data;
+        setTimeout(() => setProgress(70), 100);
+        const blob = new Blob([compressedSvg], { type: 'image/svg+xml' });
+        setCompressedFile(blob);
+        setTimeout(() => setProgress(100), 300);
       };
       reader.readAsText(file);
+    }else if (['image/jpeg', 'image/png'].includes(file.type)) {
+      // TODO: Приблизиться к tinypng. Сейчас PNG 530 Кб => 418 Кб, tinypng 155 Кб
+      const options = {
+        // maxSizeMB: 1,  // Устанавливаем целевой размер файла в мегабайтах
+        useWebWorker: true,  // Используем Web Worker для улучшения производительности
+        initialQuality: 0.75,  // Уровень качества: 0.7 (приближенно к TinyPNG)
+        alwaysKeepResolution: true,  // Сохраняем разрешение изображения
+        fileType: file.type,  // Преобразуем в тот же формат, что и оригинал
+        onProgress: (p: number) => setProgress(p),  // Прогресс сжатия
+      };
+      try {
+        const compressedImage = await imageCompression(file, options);
+        setCompressedFile(compressedImage);
+      } catch {
+        setErrorMessage('Image compression error');
+      }
     } else {
-      setErrorMessage('Please drop a valid SVG file.');
-    }
-  };
-
-  const compressSvg = (svgData: string) => {
-    setCompressionProgress(10);
-
-    try {
-      const result = optimize(svgData, {
-        multipass: true, // Улучшенное сжатие без потери качества
-        floatPrecision: 2,
-      });
-
-      setTimeout(() => {
-        setCompressionProgress(70);
-      }, 100);
-
-      // TODO: Переписать анимацию на CSS
-      setTimeout(() => {
-        setCompressedSvg(result.data);
-        setCompressionProgress(100);
-      }, 300);
-    } catch {
-      setSvgContent(null);
-      setErrorMessage('Something went wrong. Check that your file is valid.');
+      setErrorMessage('Only JPEG, PNG and SVG images are supported');
     }
   };
 
   const downloadCompressedSvg = () => {
-    if (compressedSvg) {
-      const blob = new Blob([compressedSvg], { type: 'image/svg+xml' });
+    if (compressedFile) {
+      const blob = new Blob([compressedFile], { type: 'image/svg+xml' });
       const link = document.createElement('a');
       link.href = URL.createObjectURL(blob);
-      link.download = 'compressed_image.svg';
+      link.download = fileRef.current.name || 'compressed.svg';
       link.click();
     }
   };
@@ -82,19 +83,19 @@ export const App: React.FC = () => {
         onDragLeave={handleDragLeave}
         onDrop={handleDrop}
       >
-        {svgContent ? <p>SVG uploaded. Compressing...</p> : <p>Drag & drop your SVG file here</p>}
+        <p>Drag and drop an image here (SVG, JPEG, PNG)</p>
       </div>
 
-      {compressionProgress > 0 && !errorMessage && (
+      {progress > 0 && !errorMessage && !compressedFile && (
         <div className={styles.progress}>
-          <div className={styles.progressBar} style={{ width: `${compressionProgress}%` }}></div>
+          <div className={styles.progressBar} style={{ width: `${progress}%` }}></div>
         </div>
       )}
 
-      {compressedSvg && (
+      {compressedFile && (
         <div>
-          <p>Compression complete!</p>
-          <button onClick={downloadCompressedSvg}>Download Compressed SVG</button>
+          <p className={styles.success}>Compression complete!</p>
+          <button onClick={downloadCompressedSvg}>Download an Image</button>
         </div>
       )}
 
