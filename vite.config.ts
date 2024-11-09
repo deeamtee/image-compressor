f7c8bb03e3797d2c294e84e6f22ea634b84aa7e5
Сергей Константинов <iceforcy@gmail.com>
2024-11-09 20:38:53 +0300

    Удалён лишний код

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import svgr from '@svgr/rollup';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react(), svgr(), nodePolyfills()],
+  plugins: [
+    react(),
+    svgr(),
+    nodePolyfills()
+  ],
   build: {
     rollupOptions: {
       input: {
-        content: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'index.html'),
+        content: resolve(__dirname, 'src/main.tsx'),
       },
-    }
-  }
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js', 
+        chunkFileNames: 'assets/[name]-[hash].js', 
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
