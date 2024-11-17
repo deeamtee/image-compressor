cbb841e03aa01104c57be6558212643d5470a9d1
Sergey K. <mock@email.zxc>
2024-11-17 13:27:35 +0300

    Добавлен селект выбора страны

---
+++ src/CountrySelect/CountrySelect.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { Select } from '../components/Select';
+import { Icon } from '../components/Icon';
+
+export const CountrySelect = () => {
+  const [language, setLanguage] = useState<string>('RU');
+
+  const options = [
+    { value: 'RU', label: <Icon variant="RU" /> },
+    { value: 'NL', label: <Icon variant="NL" /> },
+    { value: 'US', label: <Icon variant="US" /> },
+    { value: 'CN', label: <Icon variant="CN" /> },
+    { value: 'JP', label: <Icon variant="JP" /> },
+    { value: 'DE', label: <Icon variant="DE" /> },
+  ];
+
+  return <Select options={options} value={language} onChange={setLanguage} />;
+};
---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import styles from './Select.module.css';
+
+interface Option<T> {
+  value: T;
+  label: React.ReactNode;
+}
+
+interface SelectProps<T> {
+  options: Option<T>[];
+  value: T;
+  onChange: (value: T) => void;
+}
+
+export const Select = <T extends string | number>({ options, value, onChange }: SelectProps<T>) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => setIsOpen(!isOpen);
+  const handleSelect = (option: T) => {
+    onChange(option);
+    setIsOpen(false);
+  };
+
+  const selectedOption = options.find((option) => option.value === value);
+
+  return (
+    <div className={styles.select}>
+      <div className={styles.header} onClick={handleToggle}>
+        <div className={styles.label}>{selectedOption?.label}</div>
+        <svg
+          className={`${styles.icon} ${isOpen ? styles.iconOpen : ''}`}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="16"
+          height="16"
+        >
+          <path d="M7 10l5 5 5-5H7z" />
+        </svg>
+      </div>
+      {isOpen && (
+        <ul className={styles.dropdown}>
+          {options.map((option) => (
+            <li key={option.value} className={styles.option} onClick={() => handleSelect(option.value)}>
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
