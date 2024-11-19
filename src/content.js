e9284de1196fc34a7a3f67458d70e716f8ae8e0e
Sergey K. <mock@email.zxc>
2024-11-19 20:51:39 +0300

    Настройка dnd для document и sidepanel

--- src/content.js
+++ src/content.js
@@ -4,23 +4,8 @@ document.addEventListener('dragenter', (event) => {
   });
 });
 
-document.addEventListener('dragover', (event) => {
-  chrome.runtime.sendMessage({
-    type: 'dragover',
-  });
-});
-
-document.addEventListener('dragend', (event) => {
-  chrome.runtime.sendMessage({
-    type: 'dragend',
-  });
-});
-
 document.addEventListener('dragstart', (event) => {
   chrome.runtime.sendMessage({
     type: 'dragstart',
   });
 });
-
-// проверка успешной загрузки
-console.log('content.js');
