5e3ab48dd22b055d83ff77f38b6726c3e07401ee
Sergey K. <mock@email.zxc>
2024-12-15 14:38:57 +0300

    Добавлены редиректы после установки и удаления, обновлены иконки

--- public/background.js
+++ public/background.js
@@ -1,1 +1,13 @@
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(console.error);
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.runtime.setUninstallURL(
+    'https://docs.google.com/forms/d/e/1FAIpQLSdAAV6IBIHDXTmQ13EhoUhKfxs7p4UEphGcDxw9n-rl72Jk6A/viewform?usp=sf_link'
+  );
+});
+
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install') {
+    chrome.tabs.create({ url: 'https://image-compressor-landing.vercel.app/instructions' });
+  }
+});
